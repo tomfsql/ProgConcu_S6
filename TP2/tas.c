9b2e3192d7fb84ed67c4736f5c16179b290079ee
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2026-02-13 12:14:36 +0100

    beginning of TP2

---
+++ TP2/tas.c
@@ -0,0 +1,13 @@
+#include "tas.h"
+
+void entasser(int e){
+
+}
+
+void detasser(){
+
+}
+
+void afficher(){
+
+}
---
+++ TP2/tas.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int* tab[50];
+
+void entasser(int e);
+
+void detasser();
+
+void afficher();
+
