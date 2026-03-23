e790c01c22bbcb5427c5ab7f831491f51b4a5957
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2026-03-23 14:35:51 +0100

    ex2.1 working

---
+++ 6eTP/2/redirection.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+int main()
+{
+    int tube[2];
+    int res = pipe(tube);
+    if (res == -1) {
+        perror("Création Pipe");
+        exit(-1);
+    }
+    if (fork() == 0) {
+        close(tube[1]);
+        dup2(tube[0],0);
+        close(tube[0]);
+        execlp("wc","wc",NULL);
+    } else {
+        close(tube[0]);
+        dup2(tube[1],1);
+        close(tube[1]);
+        execlp("cat","cat","/etc/passwd",NULL);
+    }
+    return 0;
+}
