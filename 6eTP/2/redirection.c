3fbfc165b3e57a308fecd3322e83651de26b1ced
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2026-03-23 15:00:29 +0100

    2.2 ok

--- 6eTP/2/redirection.c
+++ 6eTP/2/redirection.c
@@ -13,12 +13,14 @@ int main()
         close(tube[1]);
         dup2(tube[0],0);
         close(tube[0]);
-        execlp("wc","wc",NULL);
+        //execlp("wc","wc",NULL);
+        execlp("tr","tr","a-z","A-Z",NULL);
     } else {
         close(tube[0]);
         dup2(tube[1],1);
         close(tube[1]);
-        execlp("cat","cat","/etc/passwd",NULL);
+        //execlp("cat","cat","/etc/passwd",NULL);
+        execlp("cut", "cut", "-f", "1", "-d",":","/etc/passwd", NULL);
     }
     return 0;
 }
