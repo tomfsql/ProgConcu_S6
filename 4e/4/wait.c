4d8fd3d3f8c3191887c75efffdb68a1891c237d8
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2026-05-03 14:55:07 +0200

    exo 4 ok

--- 4e/4/wait.c
+++ 4e/4/wait.c
@@ -5,21 +5,32 @@
 #include <sys/wait.h>
 
 int main(int argc, char* argv[]){
-    if(argc == 1){
-        perror("Missing args");
+    int n = 0;
+    if (argc != 2){
+        perror("Missing arguments \n");
+        exit(1);
     }
-    int total = atoi(argv[1]);
-    int cpt = 0;
-    int status_fils =0;
-    while(cpt < total){
+    int success = sscanf(argv[1],"%d",&n);
+    if(!success)
+        exit(-2);
+    int status_fils = 0;
+    for(int i=0; i<n;i++){
         pid_t fils = fork();
-        if (fils != 0)
-        {
-            cpt++;
-            wait(&status_fils);
-            break;
+        if(fils == 0){
+            pid_t pere = getppid();
+            pid_t moi = getpid();
+            printf("%d - Je suis %d, mon père est %d, fork a retourné %d\n",i,moi,pere,fils);
+            sleep(2*i);
+            printf("Reprise \n");
+            exit(i);
+        }
+        else if(fils < 0){
+            perror("Fork error ");
         }
     }
-    printf("%d\n",WEXITSTATUS(status_fils));
+    for(int i=0;i<n;i++){
+        pid_t fils = wait(&status_fils);
+        printf("fils: %d, status: %d\n",fils, WEXITSTATUS(status_fils));
+    }
     return 0;
 }
