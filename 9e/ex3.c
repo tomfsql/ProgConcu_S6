9bca84c962460bda2d1b8ebc60e7c36d9365cd8d
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2026-05-03 21:23:58 +0200

    fix typo ex3

--- 9e/ex3.c
+++ 9e/ex3.c
@@ -11,18 +11,17 @@ void redirect(int signum){
     if(signum == SIGINT){
         printf("Received signal \n");
         fin = 1;
-        puts("end");
+        puts("|| END ||\n");
         _exit(0);
     }
 }
 
 int main(void)
 {
-    int fin = 0;
     memset(&act,0,sizeof(act));
     act.sa_handler = redirect;
     sigaction(SIGINT,&act,NULL);
     while(!fin){
-        printf("Background task");
+        printf("Background task \n");
     }
 }
